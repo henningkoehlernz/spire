f92c4eadee77997329912badf75378fdbda8748f
Henning Koehler <henning.koehler.nz@gmail.com>
2019-07-20 18:27:03 +1200

    Rager option is now available as third mini-blessing

--- IroncladRager/src/main/java/NeowPatch.java
+++ IroncladRager/src/main/java/NeowPatch.java
@@ -2,13 +2,18 @@ package ironclad_rager;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.neow.NeowEvent;
 import com.megacrit.cardcrawl.neow.NeowReward;
 import com.megacrit.cardcrawl.neow.NeowReward.NeowRewardDef;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 public class NeowPatch {
@@ -16,15 +21,33 @@ public class NeowPatch {
     public static NeowReward.NeowRewardType IRONCLAD_RAGER;
 
     @SpirePatch(
-            clz = NeowReward.class,
-            method = "getRewardOptions",
-            paramtypez = {int.class}
+            clz = NeowEvent.class,
+            method = "miniBlessing",
+            paramtypez = {}
     )
     public static class RagerReward {
-        public static ArrayList<NeowRewardDef> Postfix(ArrayList<NeowRewardDef> __result, NeowReward __instance, final int category) {
-            if (category == 3)
-                __result.add(new NeowRewardDef(IRONCLAD_RAGER, "[ Channel your inner Rage ]"));
-            return __result;
+        private static final String[] optionStrings = { "Rager", "Poisoner" };
+        public static void Postfix(NeowEvent __instance) {
+            NeowReward newReward = new NeowReward(false);
+            switch ( AbstractDungeon.player.chosenClass )
+            {
+                case IRONCLAD:
+                    newReward.type = IRONCLAD_RAGER;
+                    newReward.optionLabel = optionStrings[0];
+                    break;
+                default:
+                    return;
+            }
+            // NeowEvent.reward is private - circumvent via reflection
+            try {
+                Field rewardField = NeowEvent.class.getDeclaredField("rewards");
+                rewardField.setAccessible(true);
+                ArrayList<NeowReward> rewards = (ArrayList<NeowReward>)rewardField.get(__instance);
+                rewards.add(newReward);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            __instance.roomEventText.addDialogOption("[ #gPlay #gas #g" + newReward.optionLabel + " ]");
         }
     }
 
@@ -44,6 +67,11 @@ public class NeowPatch {
                         it.remove();
                         RageStrike newStrike = new RageStrike();
                         AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(newStrike, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+                    }
+                    else if ( e instanceof com.megacrit.cardcrawl.cards.red.Defend_Red ) {
+                        it.remove();
+                        ShieldBash newDefend = new ShieldBash();
+                        AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(newDefend, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
                     }
                 }
             }
