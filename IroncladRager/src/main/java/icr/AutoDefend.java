8f280df7bf387e32863900f43f89ee518c36b3b5
Henning Koehler <henning.koehler.nz@gmail.com>
2019-08-04 13:50:27 +1200

    reworked auto-defend

--- IroncladRager/src/main/java/icr/AutoDefend.java
+++ IroncladRager/src/main/java/icr/AutoDefend.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 
@@ -20,20 +21,20 @@ public class AutoDefend extends CustomCard {
     public static final String IMG_PATH = "img/auto_defend.png";
     private static final int COST = 1;
     private static final int BLOCK = 4;
+    private static final int BLOCK_PER_ORB = 1;
 
     public AutoDefend() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 AbstractCard.CardType.SKILL, AbstractCard.CardColor.BLUE,
                 AbstractCard.CardRarity.BASIC, AbstractCard.CardTarget.SELF);
         this.block = this.baseBlock = BLOCK;
+        this.magicNumber = this.baseMagicNumber = BLOCK_PER_ORB;
         tags.add(BaseModCardTags.BASIC_DEFEND);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
-        if ( Util.isBloodied(p) )
-            AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
     }
 
     @Override
@@ -41,6 +42,34 @@ public class AutoDefend extends CustomCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeBlock(2);
+            this.upgradeMagicNumber(1);
         }
     }
+
+    private int orbCount() {
+        int count = 0;
+        for ( AbstractOrb orb : AbstractDungeon.player.orbs )
+            if ( !(orb instanceof com.megacrit.cardcrawl.orbs.EmptyOrbSlot) )
+                count++;
+        return count;
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int realBaseBlock = this.baseBlock;
+        this.baseBlock += this.magicNumber * orbCount();
+        super.calculateCardDamage(mo);
+        this.baseBlock = realBaseBlock;
+        this.isBlockModified = (this.block != this.baseBlock);
+    }
+
+    @Override
+    public void applyPowers() {
+        int realBaseBlock = this.baseBlock;
+        this.baseBlock += this.magicNumber * orbCount();
+        super.applyPowers();
+        this.baseBlock = realBaseBlock;
+        this.isBlockModified = (this.block != this.baseBlock);
+    }
+
 }
