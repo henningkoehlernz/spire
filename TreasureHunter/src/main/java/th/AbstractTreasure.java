d62ed7ae24a0fb8b272c5cd3a5b72b582d13b028
Henning Koehler <henning.koehler.nz@gmail.com>
2019-10-31 15:25:10 +1300

    checked in AbstractTreasure

---
+++ TreasureHunter/src/main/java/th/AbstractTreasure.java
@@ -0,0 +1,17 @@
+package th;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import basemod.abstracts.CustomCard;
+
+public abstract class AbstractTreasure extends CustomCard {
+
+    AbstractTreasure(String id, String name, String img, int cost, String rawDescription, CardTarget target) {
+        super(id, name, img, cost, rawDescription, TreasurePatch.TREASURE, CardColor.COLORLESS, CardRarity.RARE, target);
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+    }
+
+}
