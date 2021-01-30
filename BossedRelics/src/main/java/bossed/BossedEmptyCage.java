640354a832f5550748f850a23d4b6b673fc45ac0
Henning Koehler <henning.koehler.nz@gmail.com>
2021-01-31 02:52:09 +1300

    bossed Empty Cage

---
+++ BossedRelics/src/main/java/bossed/BossedEmptyCage.java
@@ -0,0 +1,44 @@
+package bossed;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.EmptyCage;
+
+public class BossedEmptyCage {
+
+    @SpirePatch(
+            clz = EmptyDeckShuffleAction.class,
+            method = SpirePatch.CONSTRUCTOR,
+            paramtypez = {}
+    )
+    public static class EmptyCageDeckShuffle {
+        public static void Postfix(EmptyDeckShuffleAction __instance) {
+            AbstractRelic relic = AbstractDungeon.player.getRelic(EmptyCage.ID);
+            if ( relic != null && !relic.usedUp ) {
+                relic.usedUp = true;
+                relic.flash();
+                AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(1));
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = AbstractPlayer.class,
+            method = "applyStartOfTurnRelics",
+            paramtypez = {}
+    )
+    public static class ApplyStartOfTurnRelics {
+        public static void Postfix(AbstractPlayer __instance) {
+            AbstractRelic relic = __instance.getRelic(EmptyCage.ID);
+            if ( relic != null ) {
+                relic.usedUp = false;
+            }
+        }
+    }
+
+}
