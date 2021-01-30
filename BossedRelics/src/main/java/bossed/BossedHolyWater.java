25a6950102dff17643a4f3d6802a1883d3ef25de
Henning Koehler <henning.koehler.nz@gmail.com>
2021-01-31 00:37:30 +1300

    bossed Holy Water

---
+++ BossedRelics/src/main/java/bossed/BossedHolyWater.java
@@ -0,0 +1,29 @@
+package bossed;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.HolyWater;
+
+public class BossedHolyWater {
+
+    @SpirePatch(
+            clz = Miracle.class,
+            method = "use",
+            paramtypez = {AbstractPlayer.class, AbstractMonster.class}
+    )
+    public static class UseMiracle {
+        public static void Postfix(Miracle __instance, AbstractPlayer p, AbstractMonster m) {
+            AbstractRelic relic = p.getRelic(HolyWater.ID);
+            if ( relic != null ) {
+                relic.flash();
+                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, 1));
+            }
+        }
+    }
+
+}
