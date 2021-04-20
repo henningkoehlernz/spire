5d1668dbf4a38c35909201f131933ec46eb64850
Henning Koehler <henning.koehler.nz@gmail.com>
2021-04-20 14:53:53 +1200

    Little House triggers at most 5 times per combat

--- BossedRelics/src/main/java/bossed/BossedTinyHouse.java
+++ BossedRelics/src/main/java/bossed/BossedTinyHouse.java
@@ -11,6 +11,7 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom;
 public class BossedTinyHouse {
 
     static final int BLOCK = 3;
+    static final int REPEATS = 5;
 
     @SpirePatch(
             clz = TinyHouse.class,
@@ -29,7 +30,22 @@ public class BossedTinyHouse {
     )
     public static class GetUpdatedDescription {
         public static String Replace(TinyHouse __instance) {
-            return __instance.DESCRIPTIONS[0] + BLOCK + __instance.DESCRIPTIONS[1];
+            if ( __instance.DESCRIPTIONS.length > 2 )
+                return __instance.DESCRIPTIONS[0] + REPEATS + __instance.DESCRIPTIONS[1] + BLOCK + __instance.DESCRIPTIONS[2];
+            else // legacy localization
+                return __instance.DESCRIPTIONS[0] + BLOCK + __instance.DESCRIPTIONS[1] + " (x" + REPEATS + ")";
+        }
+    }
+
+    @SpirePatch(
+            clz = AbstractRelic.class,
+            method = "atBattleStart",
+            paramtypez = {}
+    )
+    public static class AtBattleStart {
+        public static void Postfix(AbstractRelic __instance) {
+            if ( __instance instanceof TinyHouse )
+                __instance.counter = REPEATS;
         }
     }
 
@@ -41,7 +57,8 @@ public class BossedTinyHouse {
     public static class WasHPLost {
         public static void Postfix(AbstractRelic __instance, int damageAmount) {
             if ( __instance instanceof TinyHouse ) {
-                if ( AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && damageAmount > 0 ) {
+                if ( AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && damageAmount > 0 && __instance.counter > 0 ) {
+                    __instance.counter--;
                     __instance.flash();
                     AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new MetallicizePower(AbstractDungeon.player, BLOCK), BLOCK));
                 }
