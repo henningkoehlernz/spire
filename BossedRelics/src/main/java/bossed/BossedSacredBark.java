11f42c202f8d2547ac5d048a322a8e2bd265edf8
Henning Koehler <henning.koehler.nz@gmail.com>
2021-08-04 19:32:11 +1200

    bossed Sacred Bark

---
+++ BossedRelics/src/main/java/bossed/BossedSacredBark.java
@@ -0,0 +1,38 @@
+package bossed;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.potions.PotionSlot;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.SacredBark;
+
+public class BossedSacredBark {
+
+    public static int emptyPotionSlots(AbstractPlayer p) {
+        int count = 0;
+        for ( AbstractPotion potion : p.potions ) {
+            if ( potion instanceof PotionSlot )
+                count++;
+        }
+        return count;
+    }
+
+    @SpirePatch(
+            clz = AbstractPlayer.class,
+            method = "applyStartOfTurnRelics",
+            paramtypez = {}
+    )
+    public static class ApplyStartOfTurnRelics {
+        public static void Postfix(AbstractPlayer __instance) {
+            AbstractRelic relic = __instance.getRelic(SacredBark.ID);
+            if ( relic != null && emptyPotionSlots(__instance) > 1 ) {
+                relic.flash();
+                AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(1));
+            }
+        }
+    }
+
+}
