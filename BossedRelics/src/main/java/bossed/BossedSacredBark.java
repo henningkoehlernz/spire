f1a1d865c7cfdd61b4c8b191385a63cb31b8eb55
Henning Koehler <henning.koehler.nz@gmail.com>
2021-08-13 17:01:22 +1200

    Reworked Sacred Bark

--- BossedRelics/src/main/java/bossed/BossedSacredBark.java
+++ BossedRelics/src/main/java/bossed/BossedSacredBark.java
@@ -1,8 +1,9 @@
 package bossed;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+//import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+//import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.PotionSlot;
@@ -20,6 +21,16 @@ public class BossedSacredBark {
         return count;
     }
 
+    public static int emptyPotionSlots() {
+        int count = 0;
+        for ( AbstractPotion potion : AbstractDungeon.player.potions ) {
+            if ( potion instanceof PotionSlot )
+                count++;
+        }
+        return count;
+    }
+
+    /*
     @SpirePatch(
             clz = AbstractPlayer.class,
             method = "applyStartOfTurnRelics",
@@ -32,6 +43,25 @@ public class BossedSacredBark {
                 relic.flash();
                 AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(1));
             }
+        }
+    }
+    */
+
+    @SpirePatch(
+            clz = AbstractRelic.class,
+            method = "onLoseHpLast",
+            paramtypez = {int.class}
+    )
+    public static class OnLoseHpLast {
+        public static int Postfix(int damageAmount, AbstractRelic __instance, int __originalDamageAmount) {
+            if ( __instance instanceof SacredBark && damageAmount > 0 ) {
+                int dr = Math.min(2, emptyPotionSlots());
+                if ( dr > 0 ) {
+                    __instance.flash();
+                    return Math.max(0, damageAmount - dr);
+                }
+            }
+            return damageAmount;
         }
     }
 
