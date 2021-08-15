f8138b4fe04ece02fc4be3164464433cabf7bf4c
Henning Koehler <henning.koehler.nz@gmail.com>
2021-08-15 20:05:44 +1200

    bossed Old Coin

---
+++ BossedRelics/src/main/java/bossed/BossedOldCoin.java
@@ -0,0 +1,28 @@
+package bossed;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.OldCoin;
+
+public class BossedOldCoin {
+
+    @SpirePatch(
+            clz = AbstractRelic.class,
+            method = "onGainGold",
+            paramtypez = {}
+    )
+    public static class OnGainGold {
+        private static final int BONUS_GOLD = 3;
+        public static void Postfix(AbstractRelic __instance) {
+            if ( __instance instanceof OldCoin ) {
+                __instance.flash();
+                CardCrawlGame.goldGained += BONUS_GOLD;
+                AbstractDungeon.player.gold += BONUS_GOLD;
+                __instance.counter = Math.max(0, __instance.counter) + BONUS_GOLD;
+            }
+        }
+    }
+
+}
