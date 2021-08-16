f583f07f5fd497e6accee8bee3e416e2377547d0
Henning Koehler <henning.koehler.nz@gmail.com>
2021-08-16 13:35:51 +1200

    tweaked Old Coin

--- BossedRelics/src/main/java/bossed/BossedOldCoin.java
+++ BossedRelics/src/main/java/bossed/BossedOldCoin.java
@@ -9,12 +9,24 @@ import com.megacrit.cardcrawl.relics.OldCoin;
 public class BossedOldCoin {
 
     @SpirePatch(
+            clz = OldCoin.class,
+            method = "onEquip",
+            paramtypez = {}
+    )
+    public static class OnEquip {
+        public static void Replace(OldCoin __instance) {
+            CardCrawlGame.sound.play("GOLD_GAIN");
+            AbstractDungeon.player.gainGold(250);
+        }
+    }
+
+    @SpirePatch(
             clz = AbstractRelic.class,
             method = "onGainGold",
             paramtypez = {}
     )
     public static class OnGainGold {
-        private static final int BONUS_GOLD = 3;
+        private static final int BONUS_GOLD = 5;
         public static void Postfix(AbstractRelic __instance) {
             if ( __instance instanceof OldCoin ) {
                 __instance.flash();
