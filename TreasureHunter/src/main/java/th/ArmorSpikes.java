8dae51ccf9dcd123eb101f0b07ba3fec3d34476d
Henning Koehler <henning.koehler.nz@gmail.com>
2025-12-04 02:08:55 +1300

    Armor Spikes also grant block

--- TreasureHunter/src/main/java/th/ArmorSpikes.java
+++ TreasureHunter/src/main/java/th/ArmorSpikes.java
@@ -1,15 +1,13 @@
 package th;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ThornsPower;
-
-import static th.TreasurePatch.TREASURE;
 
 public class ArmorSpikes extends AbstractTreasure {
     public static final String ID = "TH:ArmorSpikes";
@@ -23,11 +21,13 @@ public class ArmorSpikes extends AbstractTreasure {
     public ArmorSpikes() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardTarget.NONE);
         this.baseMagicNumber = this.magicNumber = 1;
+        this.baseBlock = this.block = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new ThornsPower(p, this.magicNumber), this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
     }
 
     @Override
@@ -35,6 +35,7 @@ public class ArmorSpikes extends AbstractTreasure {
         if ( !this.upgraded ) {
             this.upgradeName();
             this.upgradeMagicNumber(1);
+            this.upgradeBlock(1);
         }
     }
 
